f93b609e8d44fc13654c5cd41ca8e7319e0d6e50
邢浩东 <5487156+kuaishou1@user.noreply.gitee.com>
2023-07-02 13:14:57 +0800

    feat: 使用jira-dev-tool取代json-server提供后端服务

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { loadDevTools } from "jira-dev-tool";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+loadDevTools(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
 );
-root.render(
-  <App />
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-);
+// root.render(
+//   <App />
+//   // <React.StrictMode>
+//   //   <App />
+//   // </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
